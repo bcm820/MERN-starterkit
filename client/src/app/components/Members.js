47fabd3f90d203915e5ac7516ca4b56f0177cee7
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-02-10 00:32:40 -0500

    added axios default header setting

--- client/src/app/components/Members.js
+++ client/src/app/components/Members.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios'
 
 // On mount of protected route:
+// If token is found, headers will be set for all requests
+// If token is not found, auth headers will be deleted
 // Axios will request private data using stored token
 // JWT will decode token and use it to query MongoDB for data
 // In this case, data is just a username
@@ -12,7 +14,9 @@ class Members extends React.Component {
   componentDidMount() {
     const token = localStorage.getItem('token')
     const headers = {'x-access-token': token}
-    axios.get('/api/members', {headers: headers})
+    if (token) axios.defaults.headers.common['x-access-token'] = token
+    else delete axios.defaults.headers.common['x-access-token']
+    axios.get('/api/members')
     .then(res => this.setState({username: res.data.username}))
     .catch(err => console.log(err))
   }
